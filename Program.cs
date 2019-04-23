92e9508e83505b7fc825c82b273753b08b01c3a1
Rapiz99 <41799830+Rapiz99@users.noreply.github.com>
2019-04-23 11:48:04 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cuotasbanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
